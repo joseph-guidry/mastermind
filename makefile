08d41937aec3b02f67b389b1c381c33aa8ae6288
joseph.guidry <joseph.guidry4@gmail.com>
2017-05-18 19:49:56 -0400

    Added random number structure and update makefile

--- makefile
+++ makefile
@@ -8,9 +8,9 @@ project: project.c
 	gcc -Wall -Wextra -Wpedantic _Wwrite-strings -Wstack-usage=1024 -Wfloat-equal -Waggregate-return -Winline -o project project.c 
 
 mastermind: mastermind.c
-	gcc -Wall -Wextra -Wpedantic _Wwrite-strings -Wstack-usage=1024 -Wfloat-equal -Waggregate-return -Winline -o mastermind mastermind.c
+	gcc -Wall -Wextra -Wpedantic -Wwrite-strings -Wstack-usage=1024 -Wfloat-equal -Waggregate-return -Winline -o mastermind mastermind.c
 	
 clean:
-    rm project mastermind
+	rm project mastermind
 
 
--- mastermind.c
+++ mastermind.c
@@ -1,6 +1,59 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct guess{
+    int boxOne;
+    int boxTwo;
+    int boxThree;
+    int boxFour;
+} guess ;
+
+void
+getAnswer(struct guess *answer);
 
 int main(void)
 {
+    struct guess answer;
+
+    getAnswer(&answer);
+    printf("%d %d %d %d\n", answer.boxOne, answer.boxTwo, answer.boxThree, answer.boxFour);
     return 0;
+}
+
+void
+getAnswer(struct guess *answer)
+{
+    /*
+        Generate random numbers and store in answer structure.
+    */
+    int num = 0;
+    num =  random() % 10;
+    answer->boxOne = num;
+    while((num =  (random() % 10)))
+    {
+        if (num == answer->boxOne)
+            continue;
+        else
+        {
+            answer->boxTwo = num;
+        }
+    }
+    while((num =  (random() % 10)))
+    {
+        if ((num == answer->boxOne) || (num == answer->boxOne))
+            continue;
+        else
+        {
+            answer->boxThree = num;
+        }
+    }
+    while((num =  (random() % 10)))
+    {
+        if ((num == answer->boxOne) || (num == answer->boxOne) || (num == answer->boxThree) )
+            continue;
+        else
+        {
+            answer->boxFour = num;
+        }
+    }
 }
